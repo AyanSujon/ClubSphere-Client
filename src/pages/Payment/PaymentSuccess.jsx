df1551257d77ffd2499d80a9f817076a25e87c1f
Ayan <sujon.seo.bd@gmail.com>
2025-12-08 18:45:54 +0600

    added payment Success route

--- src/pages/Payment/PaymentSuccess.jsx
+++ src/pages/Payment/PaymentSuccess.jsx
@@ -1,6 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSearchParams } from 'react-router';
+import useAxiosSecure from '../../hooks/useAxiosSecure';
 
 const PaymentSuccess = () => {
+    const [searchParams ] = useSearchParams();
+    const sessionId = searchParams.get(`session_id`);
+    const axiosSecure = useAxiosSecure();
+    console.log(sessionId);
+
+    useEffect(()=>{
+        if(sessionId){
+            if(sessionId){
+                axiosSecure.patch(`/payment-success?session_id=${sessionId}`)
+                .then(res => {
+                    console.log(res.data);
+                })
+            }
+        }
+    }, [sessionId, axiosSecure])
+
+
+
+
+
+
+
+
+
+
+
+
     return (
         <div>
             Payment success
